632508d110c09d0b088ea17b1bc09985da25004b
Jake Shams <jshams62@gmail.com>
2019-07-06 06:32:54 -0700

    solution for Exercism pangram problem

---
+++ Exercism/pangram.py
@@ -0,0 +1,7 @@
+def is_pangram(sentence):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    sentence = sentence.lower()
+    for letter in alphabet:
+        if letter not in sentence:
+            return False
+    return True
